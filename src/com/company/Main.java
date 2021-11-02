fb891bc79ea39f9627b81dd634e1b858196fe2da
Prabhakar mandloi <85407444+0829cs181107@users.noreply.github.com>
2021-11-02 14:37:59 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("first no. is :");
+        int a = sc.nextInt();
+
+        System.out.println("first no. is :");
+        int b = sc.nextInt();
+
+        System.out.println("first no. is :");
+        int c = sc.nextInt();
+
+        int sum = a+b+c ;
+
+        System.out.println("sum of numbers is: " +sum);
+
+
+	// write your code here
+    }
+}
